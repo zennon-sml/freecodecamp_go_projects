73ea95e96e8d151fc4da5eb2a59d14f689081eda
guts <guts@localhost.localdomain>
2022-05-24 03:48:28 -0300

    finishing controller file and adding TODOs

--- go-book-crud-sql/pkg/controllers/book-controller.go
+++ go-book-crud-sql/pkg/controllers/book-controller.go
@@ -13,21 +13,21 @@ import (
 var NewBook models.Book
 
 func GetBook(w http.ResponseWriter, r *http.Request) {
-	NewBooks := models.GetAllBooks()
-	res, _ := json.Marshal(NewBooks)
+	NewBooks := models.GetAllBooks()//TODO print later
+	res, _ := json.Marshal(NewBooks)//TODO print later
 	w.Header().Set("Content-Type", "pkglication/json")
 	w.Write(res)
 }
 
 func GetBookById(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
+	vars := mux.Vars(r)//TODO print later
 	bookId := vars["bookId"]
 	ID, err := strconv.ParseInt(bookId, 0, 0)
 	if err != nil {
 		fmt.Println("error while it was parsing")
 	}
-	bookDetails, _ := models.GetById(ID)
-	res, _ := json.Marshal(bookDetails)
+	bookDetails, _ := models.GetById(ID)//TODO print later
+	res, _ := json.Marshal(bookDetails)//TODO print later
 	w.Header().Set("Content-Type", "pkglication/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
@@ -35,6 +35,51 @@ func GetBookById(w http.ResponseWriter, r *http.Request) {
 
 func CreateBook(w http.ResponseWriter, r *http.Request) {
 	CreateBook := &models.Book{}
-	utils.ParseBody(r, CreateBook)
-	b := CreateBook.CreateBook()
+	utils.ParseBody(r, CreateBook)//TODO print later
+	b := CreateBook.CreateBook()//TODO print later
+	res, _ := json.Marshal(b)
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)//TODO print later
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil{
+		fmt.Println("error while parsing to int")
+	}
+	book := models.deleteBook(ID)//TODO print later
+	res, _ := json.Marshal(book)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func UpdateBook(w http.ResponseWriter,r *http.request){
+	var updateBook = &models.Book{}//empty book variable
+	utils.ParseBody(r, UpdateBook)//gets the request and parse it to a Booktype variable that is the new book
+	vars := mux.Vars(r)//TODO print these all later and see how the work
+	bookId = vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)//gets the id of the book that will be updated
+	if err != nil{
+		fmt.Println("error while parsingInt in the update function")
+	}
+	bookDetails, db := models.GetById(ID)//TODO print later, gets the book that will be updated
+	//so UpdateBook is the new book tha will have the same id has the old book bookDetails
+
+	if updateBook.Name != ""{//if the new book paramethers have been passed right
+		bookDetails.Name = updateBook.Name//then set the atributes from bookdetails to the new ones from updateBook
+	}
+	if updateBook.Author != ""{
+		bookDetails.Author = updateBook.Author
+	}
+	if updateBook.Publication != ""{
+		bookDetails.Publication = updateBook.Publication
+	}
+	db.Save(&bookDetails)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.statusOK)
+	w.Write(res)
 }
--- go-book-crud-sql/pkg/utils/utils.go
+++ go-book-crud-sql/pkg/utils/utils.go
@@ -5,10 +5,10 @@ import (
 	"io/ioutil"
 	"net/http"
 )
-
+//it recives json and transform int something that the DB will understand
 func ParseBody(r *http.Request, x interface{}) { //function to unmarshal the json body
 	if body, err := ioutil.ReadAll(r.Body); err == nil {
-		if err := json.Unmarshal([]byte(body), x); err != nil { //molde the body aparently
+		if err := json.Unmarshal([]byte(Body), x); err != nil { //molde the body aparently
 			return
 		}
 	}
